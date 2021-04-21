5477172ef85329117130efe0a69614c3d89e7659
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-04-21 17:31:14 +0200

    add Track type to schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,1 +1,15 @@
+const { gql } = require("apollo-server");
 
+const typeDefs = gql`
+    "A track is a group of modules that teach about a specific topic"
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
+    }
+`;
+
+module.exports = typeDefs;
